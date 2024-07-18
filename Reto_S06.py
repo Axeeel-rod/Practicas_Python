051de3ea45de4fb082e1d2be47c22076fd526bd3
Axeeel-rod <axelrh14@gmail.com>
2024-07-18 01:11:35 -0600

    Reto de la semana 6

---
+++ Reto_S06.py
@@ -0,0 +1,80 @@
+def crearContraseña ():
+    print("*** Crea una contraseña que empiece con un numero ***")
+
+for _ in range (3) :
+        crearContraseña()
+
+        contraseña1 = input ("Ingresa la contraseña:")
+        contraseña2 = input ("Vuelve a ingresar la contraseña:")
+
+        if not contraseña1[0].isdigit():
+            print("La contraseña debe iniciar con un numero")
+            continue
+  
+        intentos = 3
+
+        while intentos > 0 :
+            if contraseña1 == contraseña2 :
+                print("Contraseña correcta")
+                exit()
+
+            else:
+                 print(f"Contraseña incorrecta, Intenta de nuevo te quedan {intentos - 1} intentos.")
+                 break
+        contraseña1 = input("Ingresa la contraseña:") 
+        contraseña2 = input("Vuelve a ingresar la contraseña:")
+        intentos -= 1
+        if contraseña1 == contraseña2 :
+             print("Contraseña correcta")
+             exit()
+        else:
+             contraseña1 != contraseña2 
+             print()
+
+
+ 
+     
+     
+
+ 
+
+
+  
+  
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+    
+     
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
